a12ac2a316746184240dda2216e93f64f9fbadd2
NIKHIL2007CODER <82165888+NIKHIL2007CODER@users.noreply.github.com>
2021-08-01 21:49:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,12 +17,10 @@
         leftrunningdog = loadImage("images/runningLeft.png");
         
         // other activities
-        vaccination = loadImage("images/vaccination.jpg");
         dogVaccination = loadImage("images/dogVaccination.png");
         
         //object
         dogFoodStock = loadImage("images/Food Stock.png");
-        injection = loadImage("images/injection.png");
         milk = loadImage("images/milk.png");
 
         // places
